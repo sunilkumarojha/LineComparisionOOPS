f3fb2202be5324cbc95d4dcfdc7fdf9f9520a4ba
sunilkumarojha <100593013+sunilkumarojha@users.noreply.github.com>
2022-03-30 00:03:31 +0530

    Using Opps

--- src/CompareTwoLine.java
+++ src/CompareTwoLine.java
@@ -1,3 +1,29 @@
 public class CompareTwoLine {
-    
+    public static void main(String[] args) {
+        int x1 = 2;
+        int y1 = 3;
+        int x2 = 6;
+        int y2 = 4;
+        Integer length1 = (int) Math.sqrt((x2 - x1) * 2 + (y2 - y1) * 2);
+        System.out.println("Length of First line is : " + length1);
+        int p1 = 3;
+        int q1 = 4;
+        int p2 = 7;
+        int q2 = 1;
+        Integer length2 = (int) Math.sqrt((p2 - p1) * 2 + (q2 - q1) * 2);
+        System.out.println("Length of second line is : " + length2);
+        boolean result = length1.equals(length2);
+        if (result == false)
+            System.out.println("Both Lines are not Same");
+        else
+            System.out.println("Both lines are same");
+        int result2 = length1.compareTo(length2);
+        if (result2 > 0)
+            System.out.println("Line 1 is larger than line 2");
+        else if (result2 < 0)
+            System.out.println("Line 2 is greater than line1");
+        else
+            System.out.println("Both lines are parallel");
+
+    }
 }
